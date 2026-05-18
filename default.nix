dec8253f2326715eae9392d7b2e8c81830d56cc7
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-05-18 10:43:32 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
- pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2026-05-18.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2026-01-19.tar.gz") {};
 
   rpkgs = builtins.attrValues {
     inherit (pkgs.rPackages) 
